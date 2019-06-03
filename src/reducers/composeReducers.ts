437d3bbbb4f0402eab4ad86a86f7fda20082728f
David Idol <david.idol@gmail.com>
2019-06-03 00:12:42 -0700

    Remove reverse in composeReducers

--- src/reducers/composeReducers.ts
+++ src/reducers/composeReducers.ts
@@ -4,9 +4,10 @@ export default function composeReducers<TState>(
   ...reducers: Array<Reducer<TState>>
 ): Reducer<TState> {
   return function composedReducer(state, action) {
-    const finalState = reducers
-      .reverse()
-      .reduce((updatedState, reducer) => reducer(updatedState, action), state);
+    const finalState = reducers.reduce(
+      (updatedState, reducer) => reducer(updatedState, action),
+      state,
+    );
     if (finalState === undefined) {
       throw new Error('Composed reducers yielded a final state of undefined');
     }
--- test/composeReducers.test.ts
+++ test/composeReducers.test.ts
@@ -3,12 +3,15 @@ import { AnyAction } from 'redux';
 
 describe('composeReducers', () => {
   it('executes reducers in order', () => {
-    const reducer1 = (state = '', action: AnyAction) => state + action.payload.toUpperCase();
-    const reducer2 = (state = '', action: AnyAction) => state + action.payload.toLowerCase();
+    const reducer1 = (state = '', action: AnyAction) =>
+      state + action.payload.toUpperCase();
+    const reducer2 = (state = '', _action: AnyAction) => state.toLowerCase();
     const composed = composeReducers(reducer1, reducer2);
-    expect(composed('orig', {
-      type: 'fakeAction',
-      payload: 'Data',
-    })).toEqual('origdataDATA');
+    expect(
+      composed('ORIG_', {
+        type: 'fakeAction',
+        payload: 'Data',
+      }),
+    ).toEqual('orig_data');
   });
 });
