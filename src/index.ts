41cda8cc8165f79390e0e4fd65c0b0532ff678e0
Nick Gonzalez <nicholas.g.gonzalez@gmail.com>
2019-08-25 15:02:53 -0700

    Add a useful index file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
+export * from './types/reducerConfig';
+export * from './types/crudOperation';
+export * from './config/normalizeConfig';
+export { default as getFirstDefined } from './utils/getFirstDefined';
+export { default as FetchStatus } from './models/FetchStatus';
+export { default as entityFetchReducer } from './reducers/entityFetchReducer';
+export { default as fetchStatusReducer } from './reducers/fetchStatusReducer';
+export { default as idMappedReducer } from './reducers/idMappedReducer';
+export {
+  default as paginatedQueryResultsReducer,
+} from './reducers/paginatedQueryResultsReducer';
+export { default as entityCacheReducer } from './reducers/entityCacheReducer';
+export { default as composeReducers } from './reducers/composeReducers';
+export { default as queryResultsReducer } from './reducers/queryResultsReducer';
